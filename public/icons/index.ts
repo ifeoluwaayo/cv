8d04843170752d1a21e5129038090c10d9ec8a1d
ifeoluwaayo <adeayomide112@gmail.com>
2024-01-24 14:09:01 +0100

    init

---
+++ public/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as Pin } from "./Pin";
+export { default as Github } from "./Github";
+export { default as LinkedIn } from "./LinkedIn";
+export { default as X } from "./X";
+export { default as Tel } from "./Tel";
+export { default as Mail } from "./Mail";
