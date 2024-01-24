311a7d692fc811b6d2b4173b3db070ff0e43cc95
ifeoluwaayo <adeayomide112@gmail.com>
2024-01-24 22:58:26 +0100

    okay

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -11,27 +11,27 @@ export function ToolBar() {
     <div className="fixed bottom-8 left-1/2 flex w-max -translate-x-1/2 items-center rounded-full border bg-muted/10 p-2 shadow-lg backdrop-blur *:shrink-0">
       <Status />
       <div className="flex items-center">
-        {RESUME_DATA.contact.social.map((social) => (
-          <Button
-            key={social.name}
-            className="rounded-full"
-            variant="ghost"
-            size="icon"
-            asChild
-          >
-            <a href={social.url}>
-              <social.icon className="size-5" />
-              <span className="sr-only">{social.name}</span>
-            </a>
-          </Button>
-        ))}
-      </div>
-      <div className="hidden lg:contents">
+        <div className="hidden lg:contents">
+          {RESUME_DATA.contact.social.map((social) => (
+            <Button
+              key={social.name}
+              className="rounded-full"
+              variant="ghost"
+              size="icon"
+              asChild
+            >
+              <a href={social.url}>
+                <social.icon className="size-5" />
+                <span className="sr-only">{social.name}</span>
+              </a>
+            </Button>
+          ))}
+        </div>
         <Separator orientation="vertical" className="mx-2 h-8" />
-        <LocaleToggle />
-        <ModeToggle />
-        <PrintButton />
       </div>
+      <LocaleToggle />
+      <ModeToggle />
+      <PrintButton />
     </div>
   );
 }
